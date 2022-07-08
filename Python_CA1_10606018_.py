48e8939d214b7b7d179e4ca3e477ec5226e8e395
Manjunath Prakash Shetti <108906493+Manjunath-shetti@users.noreply.github.com>
2022-07-08 02:43:53 +0100

    Add files via upload

--- Python_CA1_10606018_.py
+++ Python_CA1_10606018_.py
@@ -1,3 +1,4 @@
+import unittest
 class emp:
   def insert(selfself, Staff_Id, last_name, first_name,Reg_hours, hourly_rate, OT_multiple, Tax_credit, Standard_band):
     self.staff_Id = staff_Id
@@ -8,3 +9,15 @@ class emp:
     self.OT_multiple = OT_multiple
     self.Tax_credit = Tax_credit
     self.Standard_band = Standard_band
+
+
+
+    Employee_Details = {}
+    Employee_Details["staffId"] = staff_Id
+    Employee_Details["name"] = self.last_name+ " "+ self.first_name
+    Employee_Details["Date"] = Date
+    print(Employee_Details)
+    return Employee_Details
+
+
+
