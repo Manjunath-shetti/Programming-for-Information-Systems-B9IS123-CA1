7ab47e1565d104f054de59b1264330c7969fbd18
Manjunath Prakash Shetti <108906493+Manjunath-shetti@users.noreply.github.com>
2022-07-08 02:18:33 +0100

    Add files via upload

---
+++ Python_CA1_10606018_.py
@@ -0,0 +1,10 @@
+class emp:
+  def insert(selfself, Staff_Id, last_name, first_name,Reg_hours, hourly_rate, OT_multiple, Tax_credit, Standard_band):
+    self.staff_Id = staff_Id
+    self.last_name = last_name
+    self.first_name = first_name
+    self.Reg_hours = Reg_hours
+    self.hourly_rate = hourly_rate
+    self.OT_multiple = OT_multiple
+    self.Tax_credit = Tax_credit
+    self.Standard_band = Standard_band
